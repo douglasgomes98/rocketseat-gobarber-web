4feb8065c951c8059bd40d9ae5779091d2f4ea62
douglasgomes98 <douglasgomes.rr@gmail.com>
2019-10-29 15:45:25 -0400

    feature page signin and singout

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import Logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  name: Yup.string('O nome é obrigatório')
+    .min(6, 'No minímo seis caracteres')
+    .required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={Logo} alt="Logo" />
+
+      <Form schema={schema} onSubmit={handleSubmit} noValidate>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="email" type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Acessar</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
