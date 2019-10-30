5f8ae79e8218ab74946e5c4e8d3f27e17644ab84
douglasgomes98 <douglasgomes.rr@gmail.com>
2019-10-30 09:50:54 -0400

    add login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-import history from '~/services/history';
-import Routes from '~/routes';
 import '~/config/Reactotron';
+import Routes from '~/routes';
+import history from '~/services/history';
+
 import store from './store';
 import GlobalStyle from '~/styles/global';
 
