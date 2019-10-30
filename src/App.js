ab5fb67960f07ae72d561a5dabb18fb9d5b75a72
douglasgomes98 <douglasgomes.rr@gmail.com>
2019-10-30 11:30:32 -0400

    ajust on login

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import '~/config/Reactotron';
+import { store, persistor } from './store';
+
 import Routes from '~/routes';
 import history from '~/services/history';
 
-import { store, persistor } from './store';
 import GlobalStyle from '~/styles/global';
 
 function App() {
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,22 +1,26 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
 import api from '~/services/api';
-import { signInSucess } from './actions';
+import { signInSucess, signFailure } from './actions';
 import history from '~/services/history';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', { email, password });
+    const response = yield call(api.post, 'sessions', { email, password });
 
-  const { token, user } = response.data;
-  // console.tron.log(response.data);
-  if (!user.provider) {
-    return;
+    const { token, user } = response.data;
+
+    if (!user.provider) {
+      return;
+    }
+
+    yield put(signInSucess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSucess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
